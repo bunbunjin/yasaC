9615938f62522ab84507b602ba96879305d43ab8
bunbunjin <bunbunjin.aisu@gmail.com>
2021-11-01 22:04:18 +0900

    while

--- yasac/practice.c
+++ yasac/practice.c
@@ -4,8 +4,12 @@
 
 #include "stdio.h"
 int main(void){
-    int a;
-    for (a=1; a <= 10; ++a){
-        printf("%d\n", a);
+    int num = 0;
+    while (num){
+        printf("整数を入力してください\n");
+        scanf("%d", &num);
+        printf("%dが入力されました。\n", num);
     }
+    printf("繰り返し処理が終わりました。\n");
+    return 0;
 }
