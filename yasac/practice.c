8f00fab5316ef2bdb9f0794a12fca5547eca9ddd
bunbunjin <bunbunjin.aisu@gmail.com>
2021-11-01 21:03:05 +0900

    for

--- yasac/practice.c
+++ yasac/practice.c
@@ -4,15 +4,8 @@
 
 #include "stdio.h"
 int main(void){
-    int num1 = 1;
-    int num2 = 2;
-    int sum = num1+num2;
-
-    printf("変数num1の値は%d\n", num1);
-    printf("変数num2の値は%d\n", num2);
-    printf("変数sumの値は%d\n", sum);
-
-    num1++;
-    printf("変数num1の値に1を足すと%d\n", num1);
-    return 0;
+    int a;
+    for (a=1; a <= 10; ++a){
+        printf("%d\n", a);
+    }
 }
